3e884b6dd322d0494b6d790c99f52faa33444dbf
IslamYuks <ifares82@yahoo.com>
2022-03-02 16:46:36 +0200

    Updates

---
+++ precache-manifest.a7140c72010cc0e8cf75a28243e6dbc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426963c5a85ecc85829c772ad48b5a8e",
+    "url": "/Reminder-App/index.html"
+  },
+  {
+    "revision": "263208be714e55fd6c11",
+    "url": "/Reminder-App/static/css/2.561037eb.chunk.css"
+  },
+  {
+    "revision": "e6f7d4484bf4a1f66003",
+    "url": "/Reminder-App/static/css/main.13c3648d.chunk.css"
+  },
+  {
+    "revision": "263208be714e55fd6c11",
+    "url": "/Reminder-App/static/js/2.7395343c.chunk.js"
+  },
+  {
+    "revision": "e6f7d4484bf4a1f66003",
+    "url": "/Reminder-App/static/js/main.f7175d82.chunk.js"
+  },
+  {
+    "revision": "3bd95998296028d8313b",
+    "url": "/Reminder-App/static/js/runtime~main.c025d1f3.js"
+  },
+  {
+    "revision": "de7022b37872b74f3f3286acec4cc3c4",
+    "url": "/Reminder-App/static/media/logo.de7022b3.png"
+  }
+]);
